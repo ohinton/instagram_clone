ad23666b3027859b3529f68041690498eab59d64
Noah Yasskin and Olivia Hinton <noah@>
2016-09-15 10:56:39 -0700

    add spec test that should not edit the user information if a parameter is not entered

--- spec/features/edit_user_pages_spec.rb
+++ spec/features/edit_user_pages_spec.rb
@@ -11,17 +11,17 @@ describe 'the edit user process' do
     click_button 'Update'
     expect(page).to have_content 'Your account has been updated successfully.'
   end
-end
 
-#   it "should not edit the account information if a parameter is not entered" do
-#     user = FactoryGirl.create(:user)
-#     account = FactoryGirl.create(:account, user_id: user.id)
-#     login_as(user, :scope => :user)
-#     visit account_path(account)
-#     click_on 'Edit Profile'
-#     fill_in 'Name', :with => ''
-#     fill_in 'Zodiac', :with => ''
-#     click_on 'Update Account'
-#     expect(page).to have_content 'Edit Profile'
-#   end
-# end
+  it "should not edit the user information if a parameter is not entered" do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user)
+    visit root_path
+    click_on 'Edit Account'
+    fill_in 'Email', :with => ''
+    fill_in 'Current password', :with => ''
+    click_on 'Update'
+    expect(page).to have_content 'Email can\'t be blank'
+    expect(page).to have_content 'Current password can\'t be blank'
+
+  end
+end
